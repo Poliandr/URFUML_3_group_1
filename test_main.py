79ab09797079bf57b64ff1b031a711628e2a07eb
IKWor <149700414+IKWor@users.noreply.github.com>
2023-12-21 21:37:14 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import app
 client = TestClient(app)
 
 
-def run_classification():
+def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
