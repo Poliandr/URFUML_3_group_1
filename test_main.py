a506b2ea9c38d80206860ed297912d11b2522225
IKWor <149700414+IKWor@users.noreply.github.com>
2024-03-23 21:20:09 +0300

    Update test_main.py
    Добавлен новый тест: 
    test_classification_result() загружает тестовое изображение, запускает функцию run_classification и сравнивает полученный результат с ожидаемым результатом "cat". Если результат не соответствует ожиданиям, тест завершится с ошибкой.
    
    Исправлен стиль кода по PEP8

--- test_main.py
+++ test_main.py
@@ -2,14 +2,14 @@ from PIL import Image
 from main import run_classification
 
 def test_run_classification():
-    # загрузка тестового изображения
+    # Загрузка тестового изображения
     image = Image.open("test_image.jpg")
 
-    # вызов функции run_classification
+    # Вызов функции run_classification
     run_classification(image)
 
 def test_invalid_image_input():
-    # тест на некорректный ввод изображения
+    # Тест на некорректный ввод изображения
     invalid_image_path = "invalid_image.jpg"
     try:
         invalid_image = Image.open(invalid_image_path)
@@ -18,12 +18,20 @@ def test_invalid_image_input():
         assert type(e) == FileNotFoundError
 
 def test_output_format():
-    # тест на формат вывода функции run_classification
+    # Тест на формат вывода функции run_classification
     image = Image.open("test_image.jpg")
     output = run_classification(image)
     assert isinstance(output, str)
+
+def test_classification_result():
+    # Тест на корректность результата классификации
+    image = Image.open("test_image.jpg")
+    expected_result = "cat"  # Предполагаемый ожидаемый результат классификации
+    result = run_classification(image)
+    assert result == expected_result
 
 if __name__ == "__main__":
     test_run_classification()
     test_invalid_image_input()
     test_output_format()
+    test_classification_result()
