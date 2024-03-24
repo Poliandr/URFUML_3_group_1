c0cbc1933cfdb564684bc11258481b9a1cb24945
Alex Izmalkin <sndbox44@gmail.com>
2024-03-23 22:43:35 -0300

    Align code style to PEP8

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from PIL import Image
 
 def main():
     overlay_opacity = 0.5
-
     # Пользовательский CSS для изменения заднего фона
     page_bg_img = f"""
     <style>
     .main {{
         background-image: url(
-        "https://avatars.dzeninfra.ru/get-zen_doc/1718877/pub_61cacc9bd1e72b6a321017e8_62335a98f781ab27c5f97f55/scale_1200"
-        );
+            "https://avatars.dzeninfra.ru/get-zen_doc/1718877/\
+            pub_61cacc9bd1e72b6a321017e8_62335a98f781ab27c5f97f55/scale_1200");
         background-size: cover;
     }}
     .main::before {{
@@ -32,7 +31,6 @@ def main():
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
     current_page = st.session_state.get("current_page", "home")
-
     # Определение текущей страницы и её отображение
     if current_page == "home":
         home_page()
@@ -40,8 +38,9 @@ def main():
         second_page()
 
 
-def StyleButton(widget_label, font_color='white', background_color='transparent',
-                width='200px', height='60px', margin_top='10px'):             
+def StyleButton(widget_label, font_color='white',
+                background_color='transparent',
+                width='200px', height='60px', margin_top='10px'):
     """
     Функция для стилизации кнопки с использованием пользовательского CSS.
 
@@ -63,11 +62,13 @@ def StyleButton(widget_label, font_color='white', background_color='transparent'
                 if (elements[i].innerText == '{widget_label}') {{
                     elements[i].style.color = '{font_color}';
                     elements[i].style.background = '{background_color}';
-                    elements[i].style.border = '2px solid white';  // Добавляем белую границу
+                    // Добавляем белую границу
+                    elements[i].style.border = '2px solid white';
                     elements[i].style.width = '{width}';
                     elements[i].style.height = '{height}';
                     elements[i].style.margin = 'auto';  // Центрируем кнопку
-                    elements[i].style.display = 'block';  // Делаем блочным элементом для использования margin:auto
+                    // Делаем блочным элементом для использования margin:auto
+                    elements[i].style.display = 'block';
                     elements[i].style.marginTop = '{margin_top}';
                 }}
             }}
@@ -85,19 +86,18 @@ def home_page():
         font-size: 50px;
         text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.8);
         white-space: nowrap;
-        text-align: center;
-        margin-left: -4%;
+        margin-left: -15%;
     }
     .underline {
         border: 1px solid white;
-        margin-top: -30px;  /* Отрицательное значение отступа для приближения к заголовку */
-
+        /* Отрицательное значение отступа для приближения к заголовку */
+        margin-top: -30px;
     }
     .subtitle {
         color: rgba(255, 255, 255, 0.7);
         font-size: 30px;
         text-align: center;
-
+        margin-top: -10px;
         line-height: 1.2;
         font-weight: bold;
     }
@@ -108,24 +108,26 @@ def home_page():
         text-align: center;
         margin-top: 20px;
         white-space: nowrap;
-        margin-left: -18%;
+        margin-left: -25%;
     }
     </style>
     """
 
-    st.title("Classification of tourist attractions")
+    st.title("Классификация достопримечательностей")
     st.markdown(custom_css, unsafe_allow_html=True)
     st.markdown("<hr class='underline'>", unsafe_allow_html=True)
-    st.markdown("<p class='subtitle'>You have an ability to upload your photo <br> \
-                and find out the name of the tourist attraction<br> using a neural network</p>", unsafe_allow_html=True)
-    st.markdown("<p class='subtitle2'>You can upload your photo and find out the name of the tourist attraction</p>",
+    st.markdown("<p class='subtitle'>\
+                Мы определяем название достопримечательностей Москвы<br>\
+                с помощью нейросети</p>", unsafe_allow_html=True)
+    st.markdown("<p class='subtitle2'>\
+                Вы можете загрузить свою фотографию и узнать\
+                название достопримечательности</p>",
                 unsafe_allow_html=True)
-
     # Создание кнопки "Попробовать" с использованием пользовательского CSS
-    StyleButton('Test', width='250px', height='70px', margin_top='200px')
-
+    StyleButton('Попробовать', width='250px',
+                height='70px', margin_top='200px')
     # Обработка события нажатия на кнопку
-    if st.button("Test", key="home_button"):
+    if st.button("Попробовать", key="home_button"):
         st.session_state["current_page"] = "second"
         st.experimental_rerun()
 
@@ -139,12 +141,12 @@ def second_page():
         font-size: 50px;
         text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.8);
         white-space: nowrap;
-        text-align: center;
-        margin-left: -4%;
+        margin-left: -15%;
     }
     .underline {
         border: 1px solid white;
-        margin-top: -30px;  /* Отрицательное значение отступа для приближения к заголовку */
+        /* Отрицательное значение отступа для приближения к заголовку */
+        margin-top: -30px;
     }
     .subtitle3 {
         color: rgba(255, 255, 255, 0.7);
@@ -156,46 +158,46 @@ def second_page():
     }
     </style>
     """
-    st.title("Classification of tourist attractions")
+    st.title("Классификация достопримечательностей")
     st.markdown(custom_css, unsafe_allow_html=True)
     st.markdown("<hr class='underline'>", unsafe_allow_html=True)
-
-    #Загрузка фото
+    # Загрузка фото
     uploaded_file = st.file_uploader('', type=['jpg', 'jpeg', 'png'])
-    
-    #Отображение фото
+    # Отображение фото
     if uploaded_file is not None:
         st.image(uploaded_file, caption='', use_column_width=True)
         # используем PIL для открытия изображения из байтов
         image = Image.open(BytesIO(uploaded_file.read()))
 
     # Создание кнопки "Распознать" с использованием пользовательского CSS
-    StyleButton('Detect', width='250px', height='60px', margin_top='50px')
-
+    StyleButton('Распознать', width='250px', height='60px', margin_top='50px')
     # Обработка события нажатия на кнопку
-    if st.button("Detect", key="recognition_button"):
+    if st.button("Распознать", key="recognition_button"):
         # Вывод результата
-        st.markdown("<p class='subtitle3'>Detected tourist attraction:</p>",
+        st.markdown("<p class='subtitle3'>\
+                    Распознанная достопримечательность:</p>",
                     unsafe_allow_html=True)
-        # Используем модель для классификации изображения
+        # используем модель для классификации изображения
         run_classification(image)
 
 
 def run_classification(image):
-    
     """
     Функция запускает классификацию изображения с помощью модели
     и выводит результаты предсказания в приложении.
     image: PIL.Image, изображение для классификации
     """
-    
-    # Загружаем модель
-    pipe = pipeline("image-classification", model="Poliandr/moscow-attractions")
-    # Находим максимальный результат предсказания
+    # загружаем модель
+    pipe = pipeline("image-classification",
+                    model="Poliandr/moscow-attractions")
+    # находим максимальный результат предсказания
     result = max(pipe(image), key=lambda x: x['score'])
-    # Выводим результаты предсказания в приложении
+    # выводим результаты предсказания в приложении
     # st.title(result['label'])
-    st.markdown(f"<h1 style='text-align: center; margin: 0 auto;'>{result['label']}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h1 style='text-align: center;\
+                margin: 0 auto;'>{result['label']}</h1>",
+                unsafe_allow_html=True)
+
 
 if __name__ == "__main__":
     main()
